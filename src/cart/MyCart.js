5cda5881d34b84e176a1370c6008317201990435
Yesh21102001 <yeswanthkona021@gmail.com>
2024-12-23 08:36:59 +0530

    first commit

--- src/cart/MyCart.js
+++ src/cart/MyCart.js
@@ -1,159 +1,251 @@
 import React from "react";
-import { Box, Typography, Button, MenuItem, Divider } from '@mui/material';
+import { Box, Typography, Button, Grid, Divider } from "@mui/material";
 
 const MyCart = () => {
-    return (
-        <Box>
+  return (
+    <Box>
+      <Grid
+        style={{
+          backgroundColor: "#E4E0E1",
+          height: "60px",
+          display: "flex",
+          width: "100%",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Grid
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex",
+          }}
+        >
+          <Typography
+            variant="h5"
+            style={{
+              fontFamily: "Lexend, serif",
+              fontWeight: "bold",
+              marginLeft: "20px",
+              color: "#AB886D"
+            }}
+          >
+            My Cart
+          </Typography>
+        </Grid>
+      </Grid>
+      
+
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-around",
+          marginTop: "30px",
+          marginBottom: "30px",
+        }}
+      >
+        <Box
+          sx={{
+            height: "300px",
+            width: "700px",
+            borderRadius: "10px",
+            backgroundColor: "#E4E0E1",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-around",
+            }}
+          >
             <Box>
-                <Typography variant="h6" sx={{ marginTop: "30px", fontFamily: "Lexend, serif" }}>My Cart</Typography>
+              <img
+                src="https://img.freepik.com/free-photo/scandinavian-vintage-wood-cabinet-with-chair-by-dark-blue-wall_53876-98164.jpg?t=st=1734773331~exp=1734776931~hmac=364458b1c6fb12bd4656d78b4b5775578706c67716898f67e6e4c34e50af5c57&w=740"
+                style={{
+                  height: "200px",
+                  width: "200px",
+                  marginTop: "20px",
+                  borderRadius: "5px",
+                  marginLeft: "-50px",
+                }}
+              />
             </Box>
-
-            <Box sx={{ display: "flex", justifyContent: "space-around", marginTop: "30px", marginBottom: "30px" }}>
-
-                <Box sx={{
-                    height: "300px",
-                    width: "700px",
-                    borderRadius: "10px",
-                    backgroundColor: "#E4E0E1",
-
-                }}>
-                    <Box sx={{
-                        display: "flex",
-                        justifyContent: "space-around"
-                    }}>
-                        <Box>
-                            <img src="https://img.freepik.com/free-photo/scandinavian-vintage-wood-cabinet-with-chair-by-dark-blue-wall_53876-98164.jpg?t=st=1734773331~exp=1734776931~hmac=364458b1c6fb12bd4656d78b4b5775578706c67716898f67e6e4c34e50af5c57&w=740"
-                                style={{
-                                    height: "200px",
-                                    width: "200px",
-                                    marginTop: "20px",
-                                    borderRadius: "5px",
-                                    marginLeft: "-50px"
-                                }} />
-                        </Box>
-                        <Box sx={{
-                            display: "flex",
-                            flexDirection: "column",
-                            alignItems: "flex-start",
-                            marginTop: "20px",
-                            marginLeft: "-50px"
-                        }}>
-                            <Box>
-                                <Typography variant="h6">Name</Typography>
-                            </Box>
-                            <Box>
-                                <Typography variant="h6">Cost</Typography>
-                            </Box>
-                            <Box>
-                                <Typography sx={{ color: "green" }}>InStock</Typography>
-                            </Box>
-                            <Box>
-                                <Typography variant="h6" >Category Name</Typography>
-                            </Box>
-                            <Box>
-                                <Typography variant="h6" >Quantity</Typography>
-                            </Box>
-                        </Box>
-                    </Box>
-                    <Divider sx={{ marginTop: "10px" }} />
-
-                    <Box sx={{ display: "flex", justifyContent: "space-around" }}>
-                        <Box>
-                            <Button
-                                variant="contained"
-                                sx={{
-                                    fontFamily: "Lexend, serif",
-                                    background: "linear-gradient(to right, #D6C0B3, #493628)",
-                                    transition: "background-color 0.3s ease",
-                                    width: "200px",
-                                    marginTop: "10px",
-                                    '&:hover': {
-                                        background: "#493628",
-                                    },
-                                }}
-                            >
-                                Add To Wishlist
-                            </Button>
-                        </Box>
-                        <Box>
-                            <Button
-                                variant="contained"
-                                sx={{
-                                    fontFamily: "Lexend, serif",
-                                    background: "linear-gradient(to right, #D6C0B3, #493628)",
-                                    width: "200px",
-                                    transition: "background-color 0.3s ease",
-                                    marginTop: "10px",
-                                    '&:hover': {
-                                        background: "#493628",
-                                    },
-                                }}
-                            >
-                                Remove
-                            </Button>
-                        </Box>
-                    </Box>
-                </Box>
-
-                <Box sx={{
-                    height: "300px",
-                    width: "300px",
-                    borderRadius: "5px",
-                    backgroundColor: "#E4E0E1",
-                }}>
-                    <Box>
-                        <Typography variant="h6"
-                            sx={{
-                                marginTop: "20px",
-                                fontFamily: "Lexend, serif",
-                                color: "#493628"
-                            }}>Order Summary</Typography>
-                    </Box>
-                    <Box sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        gap: "20px",
-                        alignItems: "flex-start"
-                    }}>
-                        <Box>
-                            <Typography variant="h6" sx={{ marginTop: "20px", fontFamily: "Lexend, serif", marginLeft: "20px", color: "#AB886D" }}>Total Products</Typography>
-                        </Box>
-                        <Box>
-                            <Typography variant="h6" sx={{ fontFamily: "Lexend, serif", marginLeft: "20px", color: "#AB886D" }}>Total Cost</Typography>
-                        </Box>
-                    </Box>
-
-                    <Box>
-                        <Button
-                            variant="contained"
-                            sx={{
-                                fontFamily: "Lexend, serif",
-                                background: "#493628",
-                                transition: "background-color 0.3s ease",
-                                marginTop: "10px",
-                                '&:hover': {
-                                    background: "linear-gradient(to right, #D6C0B3, #493628)",
-                                },
-                            }}
-                        >
-                            Proceed To Pay
-                        </Button>
-                    </Box>
-                    <Box sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "flex-start",
-                        marginTop: "10px",
-                        marginLeft: "10px"
-                    }}>
-                        <Typography variant="h6" sx={{ fontFamily: "Lexend, serif", color: "#AB886D" }}>Any Queries?</Typography>
-                        <Typography sx={{ fontFamily: "Lexend, serif", color: "#AB886D" }}>Please Contact us to 1234567890</Typography>
-                    </Box>
-                </Box>
-
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start",
+                marginTop: "30px",
+                marginLeft: "-50px",
+              }}
+            >
+              <Box>
+                <Typography
+                  variant="h5"
+                  sx={{ fontFamily: "Lexend, serif", fontWeight: "bold" }}
+                >
+                  Name
+                </Typography>
+              </Box>
+              <Box>
+                <Typography variant="h6" sx={{ fontFamily: "Lexend, serif" }}>
+                  Cost
+                </Typography>
+              </Box>
+              <Box>
+                <Typography
+                  sx={{ color: "green", fontFamily: "Lexend, serif" }}
+                >
+                  InStock
+                </Typography>
+              </Box>
+              <Box>
+                <Typography variant="h6" sx={{ fontFamily: "Lexend, serif" }}>
+                  Category Name
+                </Typography>
+              </Box>
+              <Box>
+                <Typography variant="h6" sx={{ fontFamily: "Lexend, serif" }}>
+                  Quantity
+                </Typography>
+              </Box>
             </Box>
+          </Box>
+          <Divider sx={{ marginTop: "10px" }} />
+
+          <Box sx={{ display: "flex", justifyContent: "space-around" }}>
+            <Box>
+              <Button
+                variant="contained"
+                sx={{
+                  fontFamily: "Lexend, serif",
+                  background: "linear-gradient(to right, #D6C0B3, #493628)",
+                  transition: "background-color 0.3s ease",
+                  width: "200px",
+                  marginTop: "10px",
+                  "&:hover": {
+                    background: "#493628",
+                  },
+                }}
+              >
+                Add To Wishlist
+              </Button>
+            </Box>
+            <Box>
+              <Button
+                variant="contained"
+                sx={{
+                  fontFamily: "Lexend, serif",
+                  background: "linear-gradient(to right, #D6C0B3, #493628)",
+                  width: "200px",
+                  transition: "background-color 0.3s ease",
+                  marginTop: "10px",
+                  "&:hover": {
+                    background: "#493628",
+                  },
+                }}
+              >
+                Remove
+              </Button>
+            </Box>
+          </Box>
         </Box>
-    )
-}
+
+        <Box
+          sx={{
+            height: "300px",
+            width: "300px",
+            borderRadius: "5px",
+            backgroundColor: "#E4E0E1",
+          }}
+        >
+          <Box>
+            <Typography
+              variant="h6"
+              sx={{
+                marginTop: "20px",
+                fontFamily: "Lexend, serif",
+                color: "#493628",
+              }}
+            >
+              Order Summary
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "20px",
+              alignItems: "flex-start",
+            }}
+          >
+            <Box>
+              <Typography
+                variant="h6"
+                sx={{
+                  marginTop: "20px",
+                  fontFamily: "Lexend, serif",
+                  marginLeft: "20px",
+                  color: "#AB886D",
+                }}
+              >
+                Total Products
+              </Typography>
+            </Box>
+            <Box>
+              <Typography
+                variant="h6"
+                sx={{
+                  fontFamily: "Lexend, serif",
+                  marginLeft: "20px",
+                  color: "#AB886D",
+                }}
+              >
+                Total Cost
+              </Typography>
+            </Box>
+          </Box>
+
+          <Box>
+            <Button
+              variant="contained"
+              sx={{
+                fontFamily: "Lexend, serif",
+                background: "#493628",
+                transition: "background-color 0.3s ease",
+                marginTop: "10px",
+                "&:hover": {
+                  background: "linear-gradient(to right, #D6C0B3, #493628)",
+                },
+              }}
+            >
+              Proceed To Pay
+            </Button>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+              marginTop: "10px",
+              marginLeft: "10px",
+            }}
+          >
+            <Typography
+              variant="h6"
+              sx={{ fontFamily: "Lexend, serif", color: "#AB886D" }}
+            >
+              Any Queries?
+            </Typography>
+            <Typography sx={{ fontFamily: "Lexend, serif", color: "#AB886D" }}>
+              Please Contact us to 1234567890
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 
 export default MyCart;
--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -2,83 +2,206 @@ import React from "react";
 import { Box, Divider, Typography, Button } from "@mui/material";
 
 const Profile = () => {
-    return (
-        <Box sx={{ marginTop: "20px", display: "flex", flexDirection: "column", alignItems: "center" }}>
+  return (
+    <Box
+      sx={{
+        marginTop: "20px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      {/* Profile Container */}
+      <Box
+        sx={{
+          border: "1px solid #AB886D",
+          height: "auto",
+          width: "800px",
+          marginTop: "20px",
+          marginBottom: "20px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: "20px",
+        }}
+      >
+        {/* Profile Title */}
+        <Typography
+          variant="h5"
+          sx={{
+            fontWeight: "bold",
+            marginTop: "20px",
+            color: "#AB886D",
+            fontFamily: "Lexend, serif",
+          }}
+        >
+          Profile Details
+        </Typography>
+        <Divider
+          sx={{
+            width: "90%",
+            my: 1,
+            backgroundColor: "#AB886D",
+          }}
+        />
 
-            {/* Profile Container */}
-            <Box
-                sx={{
-                    border: "1px solid #AB886D",
-                    height: "auto",
-                    width: "800px",
-                    marginTop: "20px",
-                    marginBottom: "20px",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    padding: "20px",
-                }}
-            >
-                {/* Profile Title */}
-                <Typography variant="h5" sx={{ fontWeight: "bold", marginTop: "20px" }}>
-                    Profile Details
-                </Typography>
-                <Divider
-                    sx={{
-                        width: "90%",
-                        my: 1,
-                        backgroundColor: "#AB886D",
-                    }}
-                />
+        {/* Profile Details */}
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "30px",
+            gap: "20px",
+          }}
+        >
+          {/* Label Column */}
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-end",
+              marginRight: "20px",
+            }}
+          >
+            <Typography variant="h6" sx={{ fontFamily: "Lexend, serif" }}>
+              Full Name:
+            </Typography>
+            <Typography variant="h6" sx={{ fontFamily: "Lexend, serif" }}>
+              Mobile Number:
+            </Typography>
+            <Typography variant="h6" sx={{ fontFamily: "Lexend, serif" }}>
+              Email:
+            </Typography>
+            <Typography variant="h6" sx={{ fontFamily: "Lexend, serif" }}>
+              Gender:
+            </Typography>
+          </Box>
 
-                {/* Profile Details */}
-                <Box
-                    sx={{
-                        display: "flex",
-                        justifyContent: "center", 
-                        alignItems: "center", 
-                        marginTop: "30px",
-                        gap: "20px",
-                    }}
-                >
-                    {/* Label Column */}
-                    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "flex-end", marginRight: "20px" }}>
-                        <Typography variant="h6">Full Name:</Typography>
-                        <Typography variant="h6">Mobile Number:</Typography>
-                        <Typography variant="h6">Email:</Typography>
-                        <Typography variant="h6">Gender:</Typography>
-                    </Box>
-
-                    {/* Value Column */}
-                    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
-                        <Typography variant="h6">John Doe</Typography>
-                        <Typography variant="h6">123-456-7890</Typography>
-                        <Typography variant="h6">john.doe@example.com</Typography>
-                        <Typography variant="h6">Male</Typography>
-                    </Box>
-
-                </Box>
-                <Box sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                }}>
-                    <Button variant="contained" sx={{ width: "400px" }}>Edit</Button>
-                </Box>
-
-                <Divider
-                    sx={{
-                        width: "90%",
-                        my: 1,
-                        backgroundColor: "#AB886D",
-                        marginTop: "20px"
-                    }}
-                />
-            </Box>
-
-
+          {/* Value Column */}
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+            }}
+          >
+            <Typography variant="h6" sx={{ fontFamily: "Lexend, serif" }}>
+              John Doe
+            </Typography>
+            <Typography variant="h6" sx={{ fontFamily: "Lexend, serif" }}>
+              123-456-7890
+            </Typography>
+            <Typography variant="h6" sx={{ fontFamily: "Lexend, serif" }}>
+              john.doe@example.com
+            </Typography>
+            <Typography variant="h6" sx={{ fontFamily: "Lexend, serif" }}>
+              Male
+            </Typography>
+          </Box>
         </Box>
-    );
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "10px",
+          }}
+        >
+          <Button
+            variant="contained"
+            sx={{
+              width: "400px",
+              fontFamily: "Lexend, serif",
+              background: "linear-gradient(to right, #D6C0B3, #493628)",
+              transition: "background-color 0.3s ease",
+              "&:hover": {
+                background: "#493628",
+              },
+            }}
+          >
+            Edit
+          </Button>
+        </Box>
+
+        <Divider
+          sx={{
+            width: "90%",
+            my: 1,
+            backgroundColor: "#AB886D",
+            marginTop: "20px",
+          }}
+        />
+
+        <Box>
+          <Typography
+            variant="h5"
+            sx={{
+              fontWeight: "bold",
+              marginTop: "20px",
+              color: "#AB886D",
+              fontFamily: "Lexend, serif",
+            }}
+          >
+            Orders
+          </Typography>
+        </Box>
+
+        <Box>
+          <Box
+            sx={{
+              width: "700px",
+              height: "250px",
+              backgroundColor: "#E4E0E1",
+              borderRadius: "5px",
+              marginTop: "10px",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start",
+                marginLeft: "20px",
+                marginTop: "40px",
+              }}
+            >
+              <Typography
+                variant="h6"
+                sx={{ color: "green", fontFamily: "Lexend, serif" }}
+              >
+                Delivered
+              </Typography>
+              <Typography sx={{ fontFamily: "Lexend, serif" }}>
+                On Wed, 21 Aug
+              </Typography>
+            </Box>
+            <Divider
+              sx={{
+                width: "90%",
+                my: 1,
+                backgroundColor: "#AB886D",
+                marginTop: "20px",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start",
+                marginLeft: "30px"
+              }}
+            />
+          </Box>
+        </Box>
+
+        <Divider
+          sx={{
+            width: "90%",
+            my: 1,
+            backgroundColor: "#AB886D",
+            marginTop: "20px",
+          }}
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default Profile;
