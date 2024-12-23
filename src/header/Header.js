17ff038d60c55584dcfedc67961ee4abb20121be
Your Name <yeswanthkona021@gmail.com>
2024-12-23 10:24:05 +0530

    first commit

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,96 +1,133 @@
 import React, { useState } from "react";
-import { Box, Typography, Menu, MenuItem } from '@mui/material';
+import { Box, Typography, IconButton, Drawer, List, ListItem, ListItemText } from "@mui/material";
 import { FaUserCircle, FaCartArrowDown, FaHeart } from "react-icons/fa";
-import { useNavigate } from 'react-router-dom';
+import MenuIcon from "@mui/icons-material/Menu";
+import { useNavigate } from "react-router-dom";
 
-const MenuDropdown = ({ label, options }) => {
+const Header = () => {
     const navigate = useNavigate();
-    const [anchorEl, setAnchorEl] = useState(null);
-    const [open, setOpen] = useState(false);
+    const [drawerOpen, setDrawerOpen] = useState(false);
 
-    const handleMouseEnter = (event) => {
-        setAnchorEl(event.currentTarget);
-        setOpen(true);
+    const handleOpenProfile = () => {
+        navigate("/profile");
+        setDrawerOpen(false);
     };
 
-    const handleMouseLeave = () => {
-        setOpen(false);
+    const handleOpenWishlist = () => {
+        navigate("/wishlist");
+        setDrawerOpen(false);
+    };
+
+    const handleOpenCart = () => {
+        navigate("/cart");
+        setDrawerOpen(false);
+    };
+
+    const handleHome = () => {
+        navigate("/");
     };
 
     return (
         <Box
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-            sx={{ display: "inline-block", marginLeft: "20px" }}
+            sx={{
+                height: "50px",
+                display: "flex",
+                alignItems: "center",
+                paddingLeft: "20px",
+                backgroundColor: "#AB886D",
+                justifyContent: "space-between",
+            }}
         >
-            <Typography variant="h6" sx={{ color: "white", fontFamily: "Lexend, serif" }}>
-                {label}
-            </Typography>
-            <Menu
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleMouseLeave}
-                MenuListProps={{
-                    onMouseLeave: handleMouseLeave,
+            {/* Logo Section */}
+            <Typography
+                variant="h5"
+                sx={{
+                    color: "white",
+                    fontWeight: "bold",
+                    fontFamily: "Lexend, serif",
+                    cursor: "pointer",
                 }}
+                onClick={handleHome}
             >
-                {options.map((option, index) => (
-                    <MenuItem key={index}>{option}</MenuItem>
-                ))}
-            </Menu>
-        </Box>
-    );
-};
+                Furniture
+            </Typography>
 
-const Header = () => {
-    const navigate = useNavigate();
-
-    const handleOpenProfile = () => {
-        navigate("/profile");
-    }
-
-    const handleOpenWishlist = () => {
-        navigate("/wishlist");
-    }
-
-    const handleOpenCart = () => {
-        navigate("/cart");
-    }
-
-    const handleHome = () => {
-        navigate("/");
-    }
-
-    return (
-        <Box sx={{
-            height: "50px",
-            display: "flex",
-            alignItems: "center",
-            paddingLeft: "20px",
-            backgroundColor: "#AB886D",
-            justifyContent: "space-between"
-        }}>
-
-            <Box>
-                <Typography
-                    variant="h5"
-                    sx={{
-                        color: "white",
-                        fontWeight: "bold",
-                        fontFamily: "Lexend, serif"
-                    }}
-                    onClick={handleHome}
+            {/* Responsive Menu */}
+            <Box sx={{ display: { xs: "flex", md: "none" }, alignItems: "center", }} >
+                <IconButton onClick={() => setDrawerOpen(true)} sx={{ color: "white" }}>
+                    <MenuIcon />
+                </IconButton>
+                <Drawer
+                    anchor="right"
+                    open={drawerOpen}
+                    onClose={() => setDrawerOpen(false)}
+                    sx={{ "& .MuiDrawer-paper": { backgroundColor: "#D6C0B3" } }}
                 >
-                    Furniture
-                </Typography>
+                    <List>
+                        <ListItem button onClick={handleOpenProfile}>
+                            <FaUserCircle style={{ marginRight: "10px" }} />
+                            <ListItemText
+                                primary="Profile"
+                                primaryTypographyProps={{ fontFamily: "Lexend, serif" }}
+                            />
+                        </ListItem>
+                        <ListItem button onClick={handleOpenWishlist}>
+                            <FaHeart style={{ marginRight: "10px" }} />
+                            <ListItemText
+                                primary="Wishlist"
+                                primaryTypographyProps={{ fontFamily: "Lexend, serif" }}
+                            />
+                        </ListItem>
+                        <ListItem button onClick={handleOpenCart}>
+                            <FaCartArrowDown style={{ marginRight: "10px" }} />
+                            <ListItemText
+                                primary="Cart"
+                                primaryTypographyProps={{ fontFamily: "Lexend, serif" }}
+                            />
+                        </ListItem>
+                    </List>
+                </Drawer>
+
             </Box>
 
-            <Box sx={{
-                marginRight: "50px"
-            }}>
-                <FaUserCircle style={{ color: "white", width: "20px", height: "20px", marginLeft: "40px" }} onClick={handleOpenProfile} />
-                <FaHeart style={{ color: "white", width: "20px", height: "20px", marginLeft: "20px" }} onClick={handleOpenWishlist} />
-                <FaCartArrowDown style={{ color: "white", width: "20px", height: "20px", marginLeft: "20px" }} onClick={handleOpenCart}/>
+            {/* Desktop Icons */}
+            <Box
+                sx={{
+                    display: { xs: "none", md: "flex" },
+                    alignItems: "center",
+                    marginRight: "50px",
+                }}
+            >
+                <FaUserCircle
+                    style={{
+                        color: "white",
+                        width: "20px",
+                        height: "20px",
+                        marginLeft: "40px",
+                        cursor: "pointer",
+                    }}
+                    onClick={handleOpenProfile}
+                />
+                <FaHeart
+                    style={{
+                        color: "white",
+                        width: "20px",
+                        height: "20px",
+                        marginLeft: "20px",
+                        cursor: "pointer",
+                    }}
+                    onClick={handleOpenWishlist}
+                />
+                <FaCartArrowDown
+                    style={{
+                        color: "white",
+                        width: "20px",
+                        height: "20px",
+                        marginLeft: "20px",
+                        cursor: "pointer",
+                    }}
+                    onClick={handleOpenCart}
+                />
             </Box>
         </Box>
     );
